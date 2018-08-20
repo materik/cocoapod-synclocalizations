75a3e371d1f29ea2f04ef133b53ccb05394400a6
Mattias Eriksson <thematerik@gmail.com>
2018-08-20 21:01:24 +0200

    [tests] use the new localizable protocol

--- Tests/Tests/Localizations/Strings.swift
+++ Tests/Tests/Localizations/Strings.swift
@@ -1,9 +1,13 @@
 
-import Foundation
+import SyncLocalizations
 
 enum Strings: String, Localizable {
 
     case title
     case description
+    
+    var tableName: String {
+        return "Localizable2"
+    }
 
 }
