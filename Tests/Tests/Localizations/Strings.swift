878a6254cc6b6c61aaf8dbb0d0e2278df3baa369
Mattias Eriksson <thematerik@gmail.com>
2018-08-22 20:03:49 +0200

    [tests] extended the tests to try out similar key name and numbers

--- Tests/Tests/Localizations/Strings.swift
+++ Tests/Tests/Localizations/Strings.swift
@@ -5,6 +5,8 @@ enum Strings: String, Localizable {
 
     case title
     case description
+    case descriptionNumber1
+    case descriptionNumber2
     
     var table: String {
         return "Localizable2"
